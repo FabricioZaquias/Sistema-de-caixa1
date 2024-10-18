f39280fa0bb1bb32905028d5661a0b19914752c0
Fabricio <fabricionomealves@gmail.com>
2024-10-18 17:34:39 -0300

    Separando utilitarios

--- src/index.js
+++ src/index.js
@@ -2,37 +2,7 @@ import React, { useState, useEffect } from "https://esm.sh/react";
 import ReactDOM from "https://esm.sh/react-dom";
 import './index.css';
 import { dadosProdutosIniciais } from "./produtos/dados";
-
-function formatarPreco(preco) {
-  return `R$ ${preco.toFixed(2)}`;
-}
-
-function calcularFrete(total, endereco) {
-  let valorFrete = 10; // Valor base do frete
-  const regiao = endereco ? endereco.uf : null;
-
-  if (total < 20) {
-    if (regiao === "RJ") {
-      valorFrete = 5;
-    } else if (regiao === "SP") {
-      valorFrete = 7;
-    } else {
-      valorFrete = 10;
-    }
-  } else {
-    valorFrete = 0; // Frete grátis acima de R$ 20
-  }
-
-  if (valorFrete > total * 0.5) {
-    valorFrete = total * 0.5;
-  }
-
-  return valorFrete;
-}
-
-function gerarNumeroPedido() {
-  return Math.floor(Math.random() * 1000000); // Gera um número aleatório de pedido
-}
+import { formatarPreco, gerarNumeroPedido, calcularFrete } from "./utilitarios/utilits";
 
 function ListaProdutos({ produtos, adicionarAoCarrinho, itensCarrinho }) {
   const [quantidades, setQuantidades] = useState({});
